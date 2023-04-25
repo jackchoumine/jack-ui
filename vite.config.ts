a6a32ce16be6c06cbde5696dd34db373be44ce95
jack <jackzhoumine@gmail.com>
2023-04-26 01:28:12 +0800

    feat: 生成类型文件

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-04-22 20:58:37
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-04-23 03:44:43
+ * @LastEditTime: 2023-04-26 01:26:25
  * @Description :
  */
 import { fileURLToPath, URL } from 'node:url'
@@ -14,7 +14,16 @@ import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 import dts from 'vite-plugin-dts'
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueSetupExtend()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueSetupExtend(),
+    // 生成dts文件 报错，但是生成了
+    dts({
+      entryRoot: 'components',
+      outputDir: 'es'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./examples', import.meta.url)),
@@ -38,15 +47,17 @@ export default defineConfig({
           format: 'cjs',
           entryFileNames: '[name].js',
           dir: 'lib',
+          // 输出模块保持原来的目录结构
           preserveModules: true,
+          // 保持原来的目录结构
           preserveModulesRoot: 'components'
         }
       ]
     },
     lib: {
-      entry: resolve('components/index.ts'),
-      name: 'jackUI',
-      formats: ['es', 'cjs']
+      entry: resolve('components/index.ts')
+      // name: 'jackUI',
+      // formats: ['es', 'cjs']
       // fileName: (format) => `index.${format}.js`
     },
     minify: false
